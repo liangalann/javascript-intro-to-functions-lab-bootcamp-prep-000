fe52153736bec4f0d56d6e7829e9bc4196d7be0d
Alan Liang <liangalann@gmail.com>
2018-03-15 21:27:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase) === lowercase
-  console.log("I can't hear you!");
- }else if (string.toUpperCase) === uppercase {
-  console.log("YES INDEED")
-}else if (string) === "I love you, Grandma."  {
-  console.log("I love you, too.")
-  }
- }
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
 }
