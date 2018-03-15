d6249c3d868b42fe304aaee0b25f8a03a7f015b0
Alan Liang <liangalann@gmail.com>
2018-03-15 20:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello'
+  lowercase.toLowercase() === lowercase
+  console.log('I can't hear you!')
+}
