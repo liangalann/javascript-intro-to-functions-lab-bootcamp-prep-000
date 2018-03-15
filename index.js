d9831cc5b705b9b1d1a438b8cd228cf6f4b94f8d
Alan Liang <liangalann@gmail.com>
2018-03-15 21:35:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
+var uppercase = "HELLO!"
+var lowercase = 'hello!'
+
 function shout(string) {
   return string.toUpperCase()
 }
 
 function whisper(string) {
   return string.toLowerCase()
-}
-
-function sayHiToGrandma(string){
-    if (string.toLowerCase() === string){
-      return 'I can\'t hear you!';
-    }
-    if (string.toUpperCase() === string){
-      return 'YES INDEED!';
-    }
-    if (string === 'I love you, Grandma.'){
-      return 'I love you, too.';
-    }
 }
